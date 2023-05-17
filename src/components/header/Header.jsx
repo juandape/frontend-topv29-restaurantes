d015161af89b8840c9e1006c88a5f871e42e674c
Juan David Peña <juandavidp76@hotmail.com>
2023-05-17 12:57:33 -0500

    feat: header and menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,29 @@
 import { NavLink } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
-// import { react } from '@babel/types';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='header-container'>
-      <nav>
-        <div className='header-container__list'>
-          <div className>
-            <NavLink to='/'> Home </NavLink>
-          </div>
-          <div>
-            <NavLink to='/about'> About </NavLink>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <nav className='header-container'>
+      <div className='header-container__logo'>@</div>
+      <div className={`header-container__items ${isOpen && 'open'}`}>
+        <div><NavLink to='/'> HOME </NavLink></div>
+        <div><NavLink to='/'> RESTAURANT </NavLink></div>
+        <div><NavLink to='/'> PAGES </NavLink></div>
+        <div><NavLink to='/about'> ABOUT </NavLink></div>
+      </div>
+      <div className={`header-container--toggle ${isOpen && 'open'}`} onClick={handleClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
   );
 }
 
