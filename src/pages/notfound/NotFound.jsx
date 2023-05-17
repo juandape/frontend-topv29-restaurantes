82e8f9aef83224c962a35833a4fa632c876214ed
Juan David Peña <juandavidp76@hotmail.com>
2023-05-16 20:35:13 -0500

    fix editor config

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-quotes */
 import { useRouteError } from 'react-router-dom';
 import React from 'react';
 import './notfound.css';
@@ -15,7 +16,11 @@ function NotFound() {
       <h1 className='container__notfound--title'>404</h1>
       <div className='container__notfound--text'>
         <p>Ups! sorry, something went wrong...</p>
-        <p> {error.statusText || error.message} </p>
+        <p>
+          {' '}
+          {error.statusText || error.message}
+          {' '}
+        </p>
       </div>
     </div>
   );
