7769c3acbb21dd2730ac94e9abc91752f8f4b4ec
Juan David Peña <juandavidp76@hotmail.com>
2023-05-16 20:33:39 -0500

    fix editor config

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -6,20 +6,16 @@ function NotFound() {
   const error = useRouteError();
 
   return (
-    <div className="container__notfound">
+    <div className='container__notfound'>
       <img
-        src="\images\dog.jpg"
-        alt="dog"
-        className="container__notfound--img"
+        src='\images\dog.jpg'
+        alt='dog'
+        className='container__notfound--img'
       />
-      <h1 className="container__notfound--title">404</h1>
-      <div className="container__notfound--text">
+      <h1 className='container__notfound--title'>404</h1>
+      <div className='container__notfound--text'>
         <p>Ups! sorry, something went wrong...</p>
-        <p>
-          {' '}
-          {error.statusText || error.message}
-          {' '}
-        </p>
+        <p> {error.statusText || error.message} </p>
       </div>
     </div>
   );
