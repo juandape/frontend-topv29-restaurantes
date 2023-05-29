f949d691e22b1976a2d198ea0814faafe17a8f3e
Juan David <41099211+juandape@users.noreply.github.com>
2023-05-29 14:45:34 -0500

    Feature/comentarios (#19)
    * feat: header and menu
    
    * feat: componente filtro comida y restaurante
    
    * fix: header
    
    * feat: readme
    
    * fix: home page
    
    * feat: filtro restaurantes mobile
    
    * feat: filtro restaurante
    
    * feat: filtro comida
    
    * feat: api and filter function
    
    * fix: css filter
    
    * fix: menu
    
    * fix: filtro busqueda
    
    * fix: filtro busqueda
    
    * fix: identacion
    
    * fix: identacion
    
    * feat: formulario registro y login
    
    * fix: forms
    
    * feat: caja comentarios

--- src/components/filtrorest/Filtrorestaurant.jsx
+++ src/components/filtrorest/Filtrorestaurant.jsx
@@ -1,34 +1,62 @@
-import React, { useState } from 'react';
-import restaurantsdata from '../../assets/restaurantsdata.json';
-
-async function getEpisodes() {
-  const response = await fetch('https://rickandmortyapi.com/api/episode/');
-  const data = await response.json();
-  return data.results;
-}
-
-const restaurants = restaurantsdata.map((item) => {
-  return item.name
-})
-
-console.log(restaurants)
+import React, { useState, useEffect } from 'react';
+import './filtrorest.css';
+import axios from 'axios';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const BASE_URL = import.meta.env.VITE_API_URL
+const url = `${BASE_URL}/api/restaurants`;
 
 function RestFilter() {
-  const [search, setSearch] = useState('')
+  const [restaurantes, setRestaurantes] = useState([]);
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    const getRestaurants = async () => {
+      const { data, status } = await axios.get(url);
+      if (status === 200) setRestaurantes(data);
+    };
+    getRestaurants();
+  }, []);
+
+  console.log(restaurantes);
 
   const handleChange = (event) => {
-    setSearch(event.target.value)
-  }
+    setSearch(event.target.value);
+    filteredRestaurants(event.target.value);
+  };
 
-  const handleClick = () => {
+  const filteredRestaurants = (searching) => {
+    const resultado = restaurantes.filter((rest) => {
+      if (
+        rest.name.toString().toLowerCase().includes(searching.toLowerCase())
+      ) {
+        return rest;
+      }
+    });
+    setRestaurantes(resultado);
+  };
 
-  }
+  const handleClick = (event) => {
+    event.preventDefault();
+    alert(restaurantes.map((rest) => rest.name));
+  };
 
   return (
-    <div>
-      <label>Busca tu restaurante  </label>
-      <input type="text" value={search} onChange={handleChange} />
-    <button onClick={handleClick}>Search</button>
+    <div className='containter__filter'>
+      {/* <label className='container__filter--label'>Busca tu restaurante  </label> */}
+      <input
+        className='container__filter--input'
+        type='text'
+        value={search}
+        placeholder='Busca tu restaurante'
+        onChange={handleChange}
+      />
+      <button
+        type='submit'
+        className='container__filter--button'
+        onClick={handleClick}
+      >
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
     </div>
   );
 }
---
+++ src/components/registerform/Regformadmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from './Registerform';
+
+function RegFormAdmin() {
+  return (
+    <div>
+      <h2 className='container__register--title'>Registro Administrador</h2>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default RegFormAdmin;
