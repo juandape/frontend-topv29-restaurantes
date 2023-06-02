83db4e9d2d168ceaf2dcfa0d043b1e4d5703d971
Juan David <41099211+juandape@users.noreply.github.com>
2023-06-02 14:33:11 -0500

    Testloginform (#35)
    * fix: nav bar
    
    * feat: pages
    
    * fix: pages
    
    * fix: pages
    
    * fix: Home.jsx
    
    * fix: home.jsx
    
    * test: loginform
    
    * fix: dependences
    
    * test: add more tests to loginform
    
    * pk
    
    * fix: conflicts
    
    * fix: loginform test
    
    * fix: loginform test

--- src/components/loginform/loginform.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import LoginForm, { defaultFormValues } from './Loginform';
-
-test('Should render login form', () => {
-  render(<LoginForm />);
-  const loginForm = 'some text'
-  expect(loginForm).toBe(true);
-});
