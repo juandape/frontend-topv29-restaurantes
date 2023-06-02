97f74ea6fd1c9169fd205eb29bae1fd73d5e8960
Juan David Peña <juandavidp76@hotmail.com>
2023-06-02 13:44:43 -0500

    test: add more tests to loginform

--- src/components/loginform/loginform.test.jsx
+++ src/components/loginform/loginform.test.jsx
@@ -1,7 +1,7 @@
 import { faHomeLgAlt, faHourglass1 } from '@fortawesome/free-solid-svg-icons';
 import { render, screen } from '@testing-library/react'
 import {describe, test, expect} from 'vitest'
-import LoginForm from './loginform'
+import LoginForm from './Loginform'
 
 
 describe('Login form test', () => {
@@ -12,4 +12,21 @@ describe('Login form test', () => {
     expect(linkElement).toBeInTheDocument();
   })
 
+  test('should render email input', () => {
+    render(<LoginForm />);
+    const linkElement = screen.getByPlaceholderText(/your email/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  test('should render password input', () => {
+    render(<LoginForm />);
+    const linkElement = screen.getByPlaceholderText(/your password/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  test('should render login button', () => {
+    render(<LoginForm />);
+    const linkElement = screen.getByText(/Login/i);
+    expect(linkElement).toBeInTheDocument();
+  })
 })
