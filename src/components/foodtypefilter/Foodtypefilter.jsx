01a1cc4091808caa6612bb6560cddcb0e3c157af
Juan David Peña <juandavidp76@hotmail.com>
2023-06-04 20:41:04 -0500

    fix bug header

--- src/components/foodtypefilter/Foodtypefilter.jsx
+++ src/components/foodtypefilter/Foodtypefilter.jsx
@@ -6,11 +6,11 @@ const url = `${BASE_URL}/api/restaurants`;
 
 function FoodTypeFilter() {
   const [foods, setFoods] = useState([]);
-  // const [isLoadingFoods, setLoadingFoods] = useState(true);
+  const [isLoadingFoods, setLoadingFoods] = useState(true);
   useEffect(() => {
     const getFood = async () => {
       const { data, status } = await axios.get(url);
-      // setLoadingFoods(false);
+      setLoadingFoods(false);
       if (status === 200) setFoods(data);
     };
     getFood();
@@ -26,68 +26,77 @@ function FoodTypeFilter() {
 
   return (
     <div>
-      {/* {isLoadingFoods
-        ? <span className='loader'>Loading...</span>
-        : ( */}
-          <div className='container__food'>
-            <button
-              className='container__food--button'
-              onClick={() => handleFoodClick('Breakfast')}
-            >
-              <label className='container__food--label' htmlFor=''>
-                Breakfast
-              </label>
-              <img
-                className='container__food--img'
-                src='/images/breakfast.jpg'
-                alt='breakfast'
-              />
-            </button>
-            <button className='container__food--button' onClick={() => handleFoodClick('Lunch')}>
-              <label className='container__food--label' htmlFor=''>
-                Lunch
-              </label>
-              <img
-                className='container__food--img'
-                src='/images/lunch.jpg'
-                alt='lunch'
-              />
-            </button>
-            <button className='container__food--button' onClick={() => handleFoodClick('Dinner')}>
-              <label className='container__food--label' htmlFor=''>
-                Dinner
-              </label>
-              <img
-                className='container__food--img'
-                src='/images/dinner.jpg'
-                alt='dinner'
-              />
-            </button>
-            <button
-              className='container__food--button'
-              onClick={() => handleFoodClick('Fast Food')}
-            >
-              <label className='container__food--label' htmlFor=''>
-                Fast Food
-              </label>
-              <img
-                className='container__food--img'
-                src='/images/fastfood.jpg'
-                alt='fast food'
-              />
-            </button>
-            <button className='container__food--button' onClick={() => handleFoodClick('Vegan')}>
-              <label className='container__food--label' htmlFor=''>
-                Vegan
-              </label>
-              <img
-                className='container__food--img'
-                src='/images/vegan.jpg'
-                alt='vegan'
-              />
-            </button>
-          </div>
-      {/* )} */}
+      {isLoadingFoods ? (
+        <span className='loader'>Loading...</span>
+      ) : (
+        <div className='container__food'>
+          <button
+            className='container__food--button'
+            onClick={() => handleFoodClick('Breakfast')}
+          >
+            <label className='container__food--label' htmlFor=''>
+              Breakfast
+            </label>
+            <img
+              className='container__food--img'
+              src='/images/breakfast.jpg'
+              alt='breakfast'
+            />
+          </button>
+          <button
+            className='container__food--button'
+            onClick={() => handleFoodClick('Lunch')}
+          >
+            <label className='container__food--label' htmlFor=''>
+              Lunch
+            </label>
+            <img
+              className='container__food--img'
+              src='/images/lunch.jpg'
+              alt='lunch'
+            />
+          </button>
+          <button
+            className='container__food--button'
+            onClick={() => handleFoodClick('Dinner')}
+          >
+            <label className='container__food--label' htmlFor=''>
+              Dinner
+            </label>
+            <img
+              className='container__food--img'
+              src='/images/dinner.jpg'
+              alt='dinner'
+            />
+          </button>
+          <button
+            className='container__food--button'
+            onClick={() => handleFoodClick('Fast Food')}
+          >
+            <label className='container__food--label' htmlFor=''>
+              Fast Food
+            </label>
+            <img
+              className='container__food--img'
+              src='/images/fastfood.jpg'
+              alt='fast food'
+            />
+          </button>
+          <button
+            className='container__food--button'
+            onClick={() => handleFoodClick('Vegan')}
+          >
+            <label className='container__food--label' htmlFor=''>
+              Vegan
+            </label>
+            <img
+              className='container__food--img'
+              src='/images/vegan.jpg'
+              alt='vegan'
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
