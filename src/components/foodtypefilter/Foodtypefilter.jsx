33e088e09dcaffb4e9ba7f87e0b293f48b478e6d
Juan David Peña <juandavidp76@hotmail.com>
2023-06-04 21:32:51 -0500

    fix: bug loading flag

--- src/components/foodtypefilter/Foodtypefilter.jsx
+++ src/components/foodtypefilter/Foodtypefilter.jsx
@@ -6,11 +6,9 @@ const url = `${BASE_URL}/api/restaurants`;
 
 function FoodTypeFilter() {
   const [foods, setFoods] = useState([]);
-  const [isLoadingFoods, setLoadingFoods] = useState(true);
   useEffect(() => {
     const getFood = async () => {
       const { data, status } = await axios.get(url);
-      setLoadingFoods(false);
       if (status === 200) setFoods(data);
     };
     getFood();
@@ -20,7 +18,7 @@ function FoodTypeFilter() {
   function handleFoodClick(foodType) {
     const food = foods.find((food) => food.foodtype === foodType);
     if (food) {
-      windows.alert(food.name);
+      alert(food.name);
     }
   }
 
