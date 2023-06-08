8e58821188019f61ebc99de9fd030a5c0ef3c611
Juan David Peña <juandavidp76@hotmail.com>
2023-06-08 14:29:37 -0500

    fix: login form access

--- src/components/access/Access.jsx
+++ src/components/access/Access.jsx
@@ -1,48 +1,25 @@
 import react, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { IoIosLogIn } from 'react-icons/io';
+import { GiArchiveRegister } from 'react-icons/gi';
 import './access.css';
 
 const Access = () => {
-  const [dropdown, setDropdown] = useState(false);
   const [dropdown1, setDropdown1] = useState(false);
-  const toggleLogin = () => setDropdown(!dropdown);
   const toggleRegister = () => setDropdown1(!dropdown1);
   return (
     <>
       <div className='container__access'>
-        <div className='container__access__login' onClick={toggleLogin}>
-          <div className='container__access__login--title'>LOGIN</div>
-          {dropdown && (
-            <div>
-              <NavLink to='/loginadmin'>
-                <div
-                  className='container__access__login--textad'
-                  role='button'
-                  onClick={toggleLogin}
-                >
-                  Admin
-                </div>
-              </NavLink>
-              <NavLink to='/loginuser'>
-                <div
-                  className='container__access__login--textus'
-                  role='button'
-                  onClick={toggleLogin}
-                >
-                  User
-                </div>
-              </NavLink>
-            </div>
-          )}
-        </div>
-
-        <div
-          className='container__access__register'
-          onClick={toggleRegister}
-        >
-          <div className='container__access__register--title'>
-            REGISTER
+        <NavLink to='/login'>
+          <div className='container__access__login'>
+            <div className='container__access__login--title'>LOGIN</div>
+            <IoIosLogIn className='container__access__login--icon' />
           </div>
+        </NavLink>
+
+        <div className='container__access__register' onClick={toggleRegister}>
+          <div className='container__access__register--title'>REGISTER</div>
+          <GiArchiveRegister className='container__access__register--icon' />
           {dropdown1 && (
             <div>
               <NavLink to='/regadmin'>
