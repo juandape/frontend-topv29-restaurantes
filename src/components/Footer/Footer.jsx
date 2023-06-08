19dbbc546763915d0c6f3d73028f876e57944921
alex.herrera <alex.guialexander@gmail.com>
2023-06-08 00:21:39 -0500

    feat: ajuste footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
+import Modal from '../modal/Modal';
 import './Footer.css';
 
 function Footer() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <footer>
+
       <div className='container_fot'>
-        <div className='fot'>
+        <div className='fot' onClick={handleClick}>
           <p className='fot_title'>RICA</p>
+         <div className={`fot_title_open ${isOpen && 'open'}`} >
           <p>Proyecto en Parejas</p>
           <p>Mayo-2023</p>
+          </div>
+
+
+
         </div>
         <div className='fot'>
-          <p className='fot_title'>About</p>
+          <p className='fot_title' onClick={handleClick}>About</p>
+          <div className={`fot_title_open1 ${isOpen && 'open'}`}>
           <p>About Us</p>
           <p>FAQ</p>
           <p>Login</p>
@@ -20,8 +35,12 @@ function Footer() {
           <p>privacy</p>
           <p>Support</p>
         </div>
+        </div>
+
+
         <div className='fot'>
-          <p className='fot_title'>Our Location</p>
+          <p className='fot_title' onClick={handleClick}>Our Location</p>
+          <div className={`fot_title_open2 ${isOpen && 'open'}`}>
           <div className='fot_mapa'>
             <a
               href='https://goo.gl/maps/kwahxaWk8zWvwk7p7'
@@ -31,10 +50,13 @@ function Footer() {
              <img src='images/mapa.jpg' alt='mapa' />
             </a>
           </div>
-
+          </div>
         </div>
+
+
         <div className='fot'>
-          <p className='fot_title'>Useful Links</p>
+          <p className='fot_title' onClick={handleClick} >Useful Links</p>
+          <div className={`fot_title_open3 ${isOpen && 'open'}`}>
           <p>Home</p>
           <p>Our Vehical</p>
           <p>Lastest Video</p>
@@ -43,12 +65,18 @@ function Footer() {
           <p>Emergency Call</p>
           <p>Mobile App</p>
         </div>
+        </div>
+
+
         <div className='fot'>
-          <p className='fot_title'>New Topics</p>
+          <p className='fot_title' onClick={handleClick} >New Topics</p>
+          <div className={`fot_title_open4 ${isOpen && 'open'}`}></div>
           <p>Abril-2023</p>
         </div>
 
         <div className='fot'>
+          <p className='fot_title' onClick={handleClick} >Diseñado por:</p>
+          <div className={`fot_title_open5 ${isOpen && 'open'}`}></div>
           <p>Diseñado Por:</p>
           <p>Guillermo Herrera</p>
           <p>Miguel Chaparro</p>
@@ -71,6 +99,8 @@ function Footer() {
         <div className='contenedorRedes_fot'>
           <p>Copyright 2023 Restaurant BY Grupo 1</p>
         </div>
+
+
       </div>
     </footer>
   );
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Modal'
+
+function Modal  ()  {
+
+  return (
+    <div className='modal'>Modal</div>
+  )
+}
+
+export default Modal;
