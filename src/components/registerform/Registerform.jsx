8fdd4be70ca692ec4236100d6265e38d8ed9a3f9
Juan David Peña <juandavidp76@hotmail.com>
2023-06-16 18:15:59 -0500

    fix: register form

--- src/components/registerform/Registerform.jsx
+++ src/components/registerform/Registerform.jsx
@@ -1,20 +1,49 @@
-import './regform.css';
 import React, { useState } from 'react';
+import './regform.css';
 
-const defaultFormValues = {
+const defaultFormData = {
   fullname: '',
   email: '',
   password: '',
   confirmPassword: '',
 };
-function RegisterForm() {
-  const [formValues, setFormValues] = useState(defaultFormValues);
 
-  //verificacion de contraseñas
+function RegisterForm() {
+  const [formData, setFormData] = useState(defaultFormData);
+  const [error, setError] = useState({});
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+  setFormData((prevData) => ({
+    ...prevData,
+    [name]: value,
+  }));
+    validateForm(event);
+  }
+
+  const validateForm = () => {
+    const { password, confirmPassword } = formData;
+    const error = {};
+    if (password.length < 6) {
+      error.password = 'Password should be at least 6 characters long';
+    }
+    if (password !== confirmPassword) {
+      error.confirmPassword = 'Passwords do not match';
+    }
+    setError(error);
+    return Object.keys(error).length === 0;
+  };
 
   function handleSubmit(event) {
     event.preventDefault();
-    setFormValues(formValues)
+    const isValid = validateForm();
+
+  if (isValid) {
+    alert('Form submitted successfully');
+    setFormData(defaultFormData);
+  } else {
+    alert('Password does not match');
+  }
 
   }
 
@@ -22,58 +51,70 @@ function RegisterForm() {
     <div className='container__register'>
       <form action='' onSubmit={handleSubmit}>
         <div className='container__register__form'>
-          <label htmlFor='' className='container__register--label'>
-            Full Name
-          </label>
-          <input
-            type='text'
-            name='fullname'
-            id='fullname'
-            className='container__register--input'
-            placeholder='your fullname'
-            value={formValues.fullname}
-            onChange={(e) => setFormValues(e.target.value)}
-            required
-          />
-          <label htmlFor='' className='container__register--label'>
-            Email
-          </label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            className='container__register--input'
-            placeholder='email@dot.com'
-            value={formValues.email}
-            onChange={(e) => setFormValues(e.target.value)}
-            required
-          />
-          <label htmlFor='' className='container__register--label'>
-            Password
-          </label>
-          <input
-            type='password'
-            name='pass'
-            id='pass'
-            className='container__register--input'
-            placeholder='your password'
-            value={formValues.password}
-            onChange={(e) => setFormValues(e.target.value)}
-            required
-          />
-          <label htmlFor='' className='container__register--label'>
-            Confirm password
-          </label>
-          <input
-            type='password'
-            name='cpass'
-            id='cpass'
-            className='container__register--input'
-            placeholder='confirm your password'
-            value={formValues.confirmPassword}
-            onChange={(e) => setFormValues(e.target.value)}
-            required
-          />
+          <div>
+            <label htmlFor='' className='container__register--label'>
+              Full Name
+            </label>
+            <input
+              type='text'
+              name='fullname'
+              id='fullname'
+              className='container__register--input'
+              placeholder='your fullname'
+              value={formData.fullname}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='' className='container__register--label'>
+              Email
+            </label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              className='container__register--input'
+              placeholder='email@dot.com'
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='' className='container__register--label'>
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              id='pass'
+              className='container__register--input'
+              placeholder='your password'
+              required
+              value={formData.password}
+              onChange={handleChange}
+              onBlur={validateForm}
+            />
+            {error.password && <span>{error.password}</span>}
+          </div>
+          <div>
+            <label htmlFor='' className='container__register--label'>
+              Confirm password
+            </label>
+            <input
+              type='password'
+              name='confirmPassword'
+              id='cpass'
+              className='container__register--input'
+              placeholder='confirm your password'
+              required
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              onBlur={validateForm}
+            />
+            {/* {error.confirmPassword && <span>{error.confirmPassword}</span>} */}
+          </div>
         </div>
         <button type='submit' className='container__register--button'>
           Register
