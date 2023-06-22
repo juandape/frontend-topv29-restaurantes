04a48d59866290a9af579738fa09bb9c23aecab9
Juan David Peña <juandavidp76@hotmail.com>
2023-06-22 08:27:34 -0500

    fix: error restaurants page

---
+++ src/pages/restaurants/Restaurants.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './restaurants.css';
+import Restaurantdetail from '../../components/restaurantdetail/Restaurantdetail';
+
+function Restaurants() {
+  return (
+    <div className='restaurants'>
+      <Restaurantdetail />
+    </div>
+  );
+}
+
+export default Restaurants;
