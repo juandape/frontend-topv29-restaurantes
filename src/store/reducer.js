eaefb95ac79d998e44bb2cc6359729ccf5e7b151
Juan David Peña <juandavidp76@hotmail.com>
2023-06-23 20:30:09 -0500

    feat: :sparkles: context archives

---
+++ src/store/reducer.js
@@ -0,0 +1,30 @@
+import { faRoad } from '@fortawesome/free-solid-svg-icons';
+import { SET_LOADING, ADD_TO_CART, SET_FOOD, REMOVE_FROM_CART } from './types';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_LOADING: {
+      return { ...state, isLoading: action.payload };
+    }
+    case SET_FOOD: {
+      return { ...state, foods: action.payload };
+    }
+    case ADD_TO_CART: {
+      const newFood = action.payload;
+      const newCart = [...state.cart, newFood];
+      const total = newCart.reduce((acc, curr) => acc + curr.price, 0);
+
+      return { ...state, cart: newCart, total };
+    }
+    case REMOVE_FROM_CART: {
+      const foodToRemove = action.payload;
+      const newCart = state.cart.filter((food) => food.id !== foodToRemove.id);
+      const total = newCart.reduce((acc, curr) => acc + curr.price, 0);
+      return { ...state, cart: newCart, total };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+export default reducer;
---
+++ src/store/types.js
@@ -0,0 +1,4 @@
+export const SET_LOADING = '@myApp/SET_LOADING';
+export const ADD_TO_CART = '@myApp/ADD_TO_CART';
+export const REMOVE_FROM_CART = '@myApp/REMOVE_FROM_CART';
+export const SET_FOOD = '@myApp/SET_FOOD';
