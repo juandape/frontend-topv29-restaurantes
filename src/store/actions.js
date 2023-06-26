848f15220f10e003cbb7c40e3cd353c3c038381b
Juan David <41099211+juandape@users.noreply.github.com>
2023-06-26 11:32:34 -0500

    Develop (#105)
    * fix:css cardrest
    
    * se actualiza el componente registro (#87)
    
    * fix: register access comp
    
    * fix: register form
    
    * fix: footer responsive (#85)
    
    Co-authored-by: alex.herrera <alex.guialexander@gmail.com>
    
    * Cardreststyle (#89)
    
    * fix: register access comp
    
    * fix: register form
    
    * fix:css access
    
    * feat: cardrest style
    
    * chore: :art: css style cardrest
    
    * feat: booking component (#90)
    
    * fix: css style home
    
    * fix: css register form (#92)
    
    * fix: div
    
    * style: :art: style access
    
    * style: footer width
    
    * fix: error restaurants page
    
    * style: booking css style
    
    * style: booking
    
    * style: css login and register
    
    * Update index.jsx
    
    * style: feedback style correction
    
    * style: :art: food filter css improve
    
    * International options en header (#96)
    
    * feat: :sparkles: international options
    
    * fix: import css
    
    * fix: responsive style
    
    * fix: delete no used functions in header
    
    * perf: :sparkles: top restaurants filter (#97)
    
    * style: improve restaurant list style (#98)
    
    * style: restaurant filter style improve
    
    * navegacion restaurant card (#102)
    
    * feat: restaurant card navigation
    
    * feat: restaurant card nav
    
    * fix: image
    
    * fix: location
    
    * fix: access css
    
    * feat: :sparkles: context archives (#99)
    
    ---------
    
    Co-authored-by: Guillermo Alexander Herrera <71296562+guialexander@users.noreply.github.com>
    Co-authored-by: alex.herrera <alex.guialexander@gmail.com>

---
+++ src/store/actions.js
@@ -0,0 +1,26 @@
+import { ADD_TO_CART, SET_LOADING, REMOVE_FROM_CART } from './types';
+
+/**
+ * Creates an action to set loading state
+ * @param {boolean} isLoading
+ * @returns
+ */
+export const setLoading = (isLoading) => ({
+  type: SET_LOADING,
+  payload: isLoading,
+});
+
+/**
+ * Creates an action to set foods
+ * @param {Object} foods
+ * @returns
+ */
+export const addFoodToCart = (food) => ({
+  type: ADD_TO_CART,
+  payload: food,
+});
+
+export const removeFoodFromCart = (food) => ({
+  type: REMOVE_FROM_CART,
+  payload: food,
+});
---
+++ src/store/index.jsx
@@ -0,0 +1,39 @@
+import { createContext, useReducer, useContext } from 'react';
+import reducer from './reducer';
+
+const AppContext = createContext();
+
+const initialState = {
+  foods: [],
+  cart: [],
+  total: 0,
+  isLoading: false,
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={[state, dispatch]}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useSelector = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('useSelector must be used within a AppProvider');
+  }
+  return context.state;
+};
+
+export const useDispatch = () => {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error('useDispatch must be used within a AppProvider');
+  }
+  return context.dispatch;
+};
