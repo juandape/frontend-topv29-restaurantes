a73d5fdfc2ed8ee77477fb1042790a604c894c8d
alex.herrera <alex.guialexander@gmail.com>
2023-07-07 21:50:59 -0500

    fix local store

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,24 +11,33 @@ import { TbLogout } from 'react-icons/tb';
 
 function Header() {
   const state = useSelector();
-  const dispatch = useDispatch();
-  // const [isLogged, setIsLogged] = useState(false);
-  const nick = state.login.profile.fullName;
+  const dispatch = useDispatch()
+
+  const nick= state.login.profile.fullName;
+
   const admin = Object.values(state.login.profile.roles[0]);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClose = () => {
-   dispatch(session(null));
-   localStorage.clear('dataUser', JSON.stringify(state.login));
-   location.reload();
-    };
+  const handleClose = () =>{
+
+    //alert("ingrese") prueba botón
+
+
+
+    localStorage.clear('dataUser', JSON.stringify(state.login));
+    location.reload();
+    dispatch( session(null));
+
+  };
+
+
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <>
+     <>
       <nav className='header-container'>
         <div className='header-container__logo'>
           <FaBiohazard />
