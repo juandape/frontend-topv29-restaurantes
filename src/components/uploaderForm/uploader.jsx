ce5cbdad6aca7a1ba6318eec19437c4160292d8d
Juan David <41099211+juandape@users.noreply.github.com>
2023-07-13 21:47:54 -0500

    Develop (#157)

--- src/components/uploaderForm/uploader.jsx
+++ src/components/uploaderForm/uploader.jsx
@@ -1,40 +1,40 @@
-import axios from "axios";
-import React, {useState} from "react";
-import { Container, FormGroup, Input } from "reactstrap";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Container, FormGroup, Input } from 'reactstrap';
 
-const UploadImage = ({dataObj,setData,name}) => {
+const BASE_URL = import.meta.env.CLOUDINARY_URL;
+const url = `${BASE_URL}/v1_1/dqrtsgiru/upload`;
 
-    const uploader = async (e) => {
-      const files = e.target.files;
-      const data = new FormData();
-      data.append("file", files[0]);
-      data.append("upload_preset", "chapitas_fail");
-      const res = await fetch(
-        "https://api.cloudinary.com/v1_1/dqrtsgiru/upload",
-        {
-          method: "POST",
-          body: data,
-        }
-      )
-        const file = await res.json();
-       setData({...dataObj,[name]:file.secure_url})
-    }
-
+const UploadImage = ({ dataObj, setData, name }) => {
+  const uploader = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'chapitas_fail');
+    const res = await fetch(url,
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const file = await res.json();
+    setData({ ...dataObj, [name]: file.secure_url });
+  };
 
   return (
     <div>
       <Container>
         <FormGroup>
           <Input
-            type="file"
-            name="file"
-            placeholder="Click Aquí"
-            onChange={ uploader }
+            type='file'
+            name='file'
+            placeholder='Click Aquí'
+            onChange={uploader}
           />
         </FormGroup>
       </Container>
     </div>
-   );
-}
+  );
+};
 
 export default UploadImage;
