71ecfbe71af6bd2d68d375d913aadf8e5545cfb4
Juan David Peña <juandavidp76@hotmail.com>
2023-07-14 10:35:55 -0500

    fix: loading

--- src/layout/root.jsx
+++ src/layout/root.jsx
@@ -1,14 +1,22 @@
 import { Outlet, useNavigation } from 'react-router-dom';
 import React from 'react';
-import Header from '../components/header/Header'
-
+import Header from '../components/header/Header';
+import UseAnimations from 'react-useanimations';
+import loading from 'react-useanimations/lib/loading';
 
 function Root() {
   const navigation = useNavigation();
+
   return (
     <>
       <Header />
-      <main>{navigation.state === 'loading' ? 'Loading...' : <Outlet />}</main>
+      <main>
+        {navigation.state === 'loading' ? (
+          <UseAnimations animation={loading} size={56} wrapperStyle={{marginTop: '100px'}} strokeColor='orange' />
+        ) : (
+          <Outlet />
+        )}
+      </main>
     </>
   );
 }
