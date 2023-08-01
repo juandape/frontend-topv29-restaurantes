411157cb51369023d691795b5f57e8c97ec115dd
Juan David Peña <juandavidp76@hotmail.com>
2023-08-01 14:25:42 -0500

    fix: publishable key

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(
-  'pk_test_51NNg21IgZKkg1b8zBdxd93m7bCg24EjScnlqVyl5vSjCHqjV3igRtvPDRaccofY5Auxj3MNps3CWdAiYsr4I3Eay00P3stSlkg'
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
